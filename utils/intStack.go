9af732dfaf957ebaa3d90f5944b2f706b149cc92
Juan Pablo R <jprscs@gmail.com>
2021-06-02 19:09:41 -0500

    Adds documentation to utils pkg

--- utils/intStack.go
+++ utils/intStack.go
@@ -1,17 +1,21 @@
 package utils
 
+// IntStack is a stack of ints
 type IntStack struct {
 	stack []int
 }
 
+// Top returns the top int of the stack
 func (is *IntStack) Top() int {
 	return is.stack[len(is.stack)-1]
 }
 
+// Push adds a new int to the stack
 func (is *IntStack) Push(n int) {
 	is.stack = append(is.stack, n)
 }
 
+// Pop removes and returns the top int of the stack
 func (is *IntStack) Pop() int {
 	result := is.Top()
 	is.stack = is.stack[:len(is.stack)-1]
--- utils/stringStack.go
+++ utils/stringStack.go
@@ -1,17 +1,21 @@
 package utils
 
+// StringStack is a stack of strings
 type StringStack struct {
 	stack []string
 }
 
+// Top returns the top string of the stack
 func (ss *StringStack) Top() string {
 	return ss.stack[len(ss.stack)-1]
 }
 
-func (ss *StringStack) Push(scope string) {
-	ss.stack = append(ss.stack, scope)
+// Push adds a string to the stack
+func (ss *StringStack) Push(str string) {
+	ss.stack = append(ss.stack, str)
 }
 
+// Removes the top string of the stack
 func (ss *StringStack) Pop() {
 	ss.stack = ss.stack[:len(ss.stack)-1]
 }
